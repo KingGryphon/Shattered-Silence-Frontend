d8e559d6fbdfbe40d19da5fd56d0640613b664ff
KingGryphon <archergryphon1508@gmail.com>
2025-01-27 12:37:52 -0500

    pictures

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+
+    const links = [
+        {href: './pages/Home.js', text: 'Home'},
+        {href: './pages/About.js', text: 'About'},
+        {href: './pages/Merch.js', text: 'Merch'},
+        {href: './pages/Tours.js', text: 'Tours'},
+        {href:'./pages/Press.js', text: 'Press Interviews'},
+        {href: './pages/Distography.js', text: 'Distorgraphy'},
+    ];
+
+    return (
+        
+    )
+};
+
+export default Header; 
